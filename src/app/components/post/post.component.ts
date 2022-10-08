fcb64d7baa7bec6613a47a0a37af3045940b63fe
Sneha Reddy <snehareddy9178@gmail.com>
2022-10-08 15:01:46 +0530

    Like functionality

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -7,12 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
+  
+  liked: boolean = false;
   img: {
     imgSrc: 'https://th.bing.com/th/id/OIP.1moOVABLsAl172i_UsaCMQHaF5?pid=ImgDet&rs=1'
-  }
-
+  };
+  heart: any = {
+    name: 'heart-outline',
+    color: 'dark'
+  };
 
   constructor() { }
 
   ngOnInit() {}
+
+  changeHeart(){
+    if(!this.liked){
+      this.liked = true;
+      this.heart.name = 'heart',
+      this.heart.color = 'danger'
+    }else{
+      this.liked = false;
+      this.heart.name = 'heart-outline',
+      this.heart.color = 'dark'
+    }
+   
+  }
 }
